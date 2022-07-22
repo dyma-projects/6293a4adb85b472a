682044b42d1272bee34050ae41c58c29aedcb7ab
Peanuts-83 <tranque@free.fr>
2022-07-22 21:50:34 +0200

    Say hello done

---
+++ chap1/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public title: string = 'chap1 - Hello!';
+  public p: string = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est, reprehenderit atque a deserunt doloribus autem quae, suscipit magnam ex, commodi expedita ut nostrum animi? Alias numquam quisquam corrupti hic velit? Bonjour Dyma ;)"
+}
